e79e4d188644af2ddd7d6805d6366eb1f03acb39
신주현 (Juhyeon Shin) <109642702+Shin363@users.noreply.github.com>
2023-02-26 17:41:20 +0900

    [level 1] Title: 자릿수 더하기, Time: 2.38 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -0,0 +1,13 @@
+function solution(n)
+{
+    var answer = 0;
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    
+    while(n > 0){
+        answer += n % 10;
+        console.log(n);
+        n = Math.floor(n/10);
+        
+    }
+    return answer;
+}
