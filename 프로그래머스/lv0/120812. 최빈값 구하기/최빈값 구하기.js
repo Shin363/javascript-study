ec65328fc69ba0a530b2b01eeb8228fe0df26c6b
신주현 (Juhyeon Shin) <109642702+Shin363@users.noreply.github.com>
2023-03-12 03:23:45 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.js
@@ -0,0 +1,16 @@
+function solution(array) {
+    var answer = 0;
+    var max=0;
+    //array 최댓값만큼 길이를 가진 배열 생성
+    var arrayList = new Array(Math.max(...array)+1).fill(0);
+    for(var i=0;i<array.length;i++){
+        arrayList[array[i]]++; //array값을 인덱스로 하는 배열 채우기
+    }
+    //arrayList의 최댓값에 해당하는 인덱스 출력
+    // 만약 indexOf와 lastIndexOf가 지칭하는 값이 같으면, -1 출력 (최빈값이 중복인 경우 확인)
+    if (arrayList.indexOf(Math.max(...arrayList)) !== arrayList.lastIndexOf(Math.max(...arrayList))) {
+        return -1;
+    } else {
+        return arrayList.indexOf(Math.max(...arrayList));
+    };
+}
