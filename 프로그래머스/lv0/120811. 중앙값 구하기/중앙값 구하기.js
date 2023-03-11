f4d1b1f96708a904ae06310b7db5c5b4bae9b28c
신주현 (Juhyeon Shin) <109642702+Shin363@users.noreply.github.com>
2023-03-12 01:29:29 +0900

    [level 0] Title: 중앙값 구하기, Time: 2.86 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.js
+++ 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.js
@@ -1,6 +1,8 @@
 function solution(array) {
     var answer = 0;
-    array.sort((a,b)=>a-b);
+    array.sort(function(a,b){
+        return a-b;
+    });
     console.log(array);
     answer=Math.floor(array.length/2);
     return array[answer];
