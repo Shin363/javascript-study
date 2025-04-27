efba2a4d276693139448a19712f76d6fce35e294
신주현 (Juhyeon Shin) <109642702+Shin363@users.noreply.github.com>
2025-04-28 00:52:17 +0900

    [level 1] Title: 대충 만든 자판, Time: 1.51 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
@@ -0,0 +1,31 @@
+function solution(keymap, targets) {
+    var answer = [];
+    var nummap = {};
+    
+    for(let i=0; i<keymap.length; i++){
+        for(let j=0; j<keymap[i].length; j++){
+            const key = keymap[i][j]
+            if(nummap[key]===undefined){
+                nummap[key]=j;
+            }
+            else if(nummap[key]>j){
+                nummap[key]=j;
+            }
+            else{continue;}
+        }
+    }
+    targets.map((word)=>{
+        let sum=0;
+        for(ch of word){
+            if(nummap[ch]!==undefined){
+                sum+=nummap[ch]+1;
+            }
+            else{
+                sum= -1;
+                break;
+            }
+        }
+        answer.push(sum);
+    });
+    return answer;
+}
