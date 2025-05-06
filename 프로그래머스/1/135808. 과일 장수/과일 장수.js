19aaefd40d9a15f62cb8b620cbad25466bd952b1
신주현 (Juhyeon Shin) <109642702+Shin363@users.noreply.github.com>
2025-05-06 23:16:59 +0900

    [level 1] Title: 과일 장수, Time: 235.60 ms, Memory: 94.6 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -2,9 +2,13 @@ function solution(k, m, score) {
     var answer = 0;
     let appleBox = [];
     score.sort((a,b)=>b-a);
-    for(let i=0; i<score.length/m; i++){
-        appleBox = score.slice(m*i,(m*i)+m);
-        if(appleBox.length<m)continue;
+    // for(let i=0; i<score.length/m; i++){
+    //     appleBox = score.slice(m*i,(m*i)+m);
+    //     if(appleBox.length<m)continue;
+    //     answer+=(Math.min(...appleBox)*m);
+    // }
+    for(let i=0; i<Math.floor(score.length/m); i++){
+        appleBox = score.slice(m*i,m*i+m);
         answer+=(Math.min(...appleBox)*m);
     }
     return answer;
