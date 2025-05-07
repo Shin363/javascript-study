97f168f5bc35e6a3d3fae345d4537ac357b5225a
신주현 (Juhyeon Shin) <109642702+Shin363@users.noreply.github.com>
2025-05-07 23:08:30 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.30 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -0,0 +1,18 @@
+function solution(food) {
+    let answer = '';
+    let leftFood = '';
+    let rightFood = '';
+    
+    for(let i=1; i<food.length; i++){
+        if(Math.floor(food[i]/2)==0)
+            continue
+        else{
+            leftFood+=i.toString().repeat(Math.floor(food[i]/2).toString());
+        }
+    }
+    for(let j=leftFood.length-1; j>=0; j--){
+        rightFood+=leftFood[j];
+    }
+    answer = leftFood + '0' + rightFood;
+    return answer;
+}
