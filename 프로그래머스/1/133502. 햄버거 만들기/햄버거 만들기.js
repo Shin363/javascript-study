1837b6cc28972693d86a909dff4b18411df50e5e
신주현 (Juhyeon Shin) <109642702+Shin363@users.noreply.github.com>
2025-05-08 19:13:44 +0900

    [level 1] Title: 햄버거 만들기, Time: 1915.44 ms, Memory: 69.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
@@ -0,0 +1,17 @@
+function solution(ingredient) {
+    var answer = 0;
+    let i=0;
+    while(i<=ingredient.length-4){
+        if(ingredient[i]==1 && ingredient[i+1]==2 &&
+          ingredient[i+2]==3 &&ingredient[i+3]==1){
+            answer++; ingredient.splice(i,4); 
+            //i를 이전 1의 위치로 돌리기
+            i=i-3<=0?0:i-3
+        }
+        else{
+            i++;
+        }
+    }
+    
+    return answer;
+}
