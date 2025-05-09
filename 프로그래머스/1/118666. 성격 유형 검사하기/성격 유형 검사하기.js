7ed919f6e66956e18240cfea4eff7f032eda6e7e
신주현 (Juhyeon Shin) <109642702+Shin363@users.noreply.github.com>
2025-05-10 02:23:10 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.52 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -1,28 +1,45 @@
 function solution(survey, choices) {
     var answer = '';
     const selectScore = [3,2,1,0,1,2,3];
-    const category = ["R","T","C","F","J","M","A","N"];
-    const myCategory = {};
-    for (let i=0; i<category.length; i++){
-        myCategory[category[i]]=0;
-    }
-    for(let i=0; i<survey.length; i++){
-        const target = choices[i]<=3?survey[i][0]:survey[i][1];
-        if(choices[i]!=4){
-            myCategory[target] += selectScore[choices[i]-1];
-        }
-        else{
-            continue;
-        }
-    }
-    for(let j=0; j<4; j++){
-        if(myCategory[category[2*j]]>=myCategory[category[2*j+1]]){
-            answer+= category[2*j];
-        }
-        else {
-            answer+= category[2*j+1];
-        }
-    }
+    const pairs = ["RT","CF","JM","AN"];
+    const categories = Object.fromEntries([..."RTCFJMAN"].map((char)=>[char,0]));
+    survey.forEach((pair,i)=>{
+        const [disagree,agree] = pair;
+        const choice = choices[i];
+        const score = selectScore[choice-1];
+        
+        if(choice < 4) categories[disagree] += score;
+        else if(choice > 4) categories[agree] += score;
+    })
     
-    return answer;
+    return pairs
+        .map(([a,b])=>
+        (categories[a] >= categories[b]? a : b))
+        .join("");
+    
+    // const category = ["R","T","C","F","J","M","A","N"];
+    // const myCategory = {};
+    // for (let i=0; i<category.length; i++){
+    //     myCategory[category[i]]=0;
+    // }
+    // for(let i=0; i<survey.length; i++){
+    //     const target = choices[i]<=3?survey[i][0]:survey[i][1];
+    //     if(choices[i]!=4){
+    //         myCategory[target] += selectScore[choices[i]-1];
+    //     }
+    //     else{
+    //         continue;
+    //     }
+    // }
+    // for(let j=0; j<4; j++){
+    //     if(myCategory[category[2*j]]>=myCategory[category[2*j+1]]){
+    //         answer+= category[2*j];
+    //     }
+    //     else {
+    //         answer+= category[2*j+1];
+    //     }
+    // }
+    
+    
+    //return answer;
 }
