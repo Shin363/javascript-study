dc38548853c2bd4d445a4ed84a0d4ef2f843757a
신주현 (Juhyeon Shin) <109642702+Shin363@users.noreply.github.com>
2025-05-13 01:10:34 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -0,0 +1,23 @@
+function isLotto(lotto){
+    if(lotto==6){return 1}
+    else if(lotto==5){return 2}
+    else if(lotto==4){return 3}
+    else if(lotto==3){return 4}
+    else if(lotto==2){return 5}
+    else{return 6}
+}
+function solution(lottos, win_nums) {
+    var answer = [0,0];
+    lottos.forEach((v)=>{
+        if(win_nums.includes(v)){
+            answer[0]++;
+            answer[1]++;
+        }
+        else if(v==0){
+            answer[0]++;
+        }
+    })
+    answer[0]=isLotto(answer[0]);
+    answer[1]=isLotto(answer[1]);
+    return answer;
+}
