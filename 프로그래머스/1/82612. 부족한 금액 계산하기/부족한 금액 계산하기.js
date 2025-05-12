d39b185b83a97856218e714096c8ada6c9fa1f90
신주현 (Juhyeon Shin) <109642702+Shin363@users.noreply.github.com>
2025-05-12 23:12:33 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.79 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
@@ -1,7 +1,8 @@
 function solution(price, money, count) {
-    var answer = 0;
-    const totalPrice = Array.from({length:count},(v,i) => price*i+price );
-    totalPrice.map((v)=>answer+=v)
-    answer = (answer-money>0)? answer-money : 0 ;
-    return answer;
+    //var answer = 0;
+    const totalPrice = Array.from({length:count},(_,i) => price*(i+1))
+    .reduce((acc,cur)=>acc+cur,0);
+    //acc:누적값 cur:현재값
+    //totalPrice.forEach((v)=>answer+=v)
+    return Math.max(totalPrice-money,0);
 }
