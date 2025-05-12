ea88c78cf640895f99e2b621ed55d12e91f76f21
신주현 (Juhyeon Shin) <109642702+Shin363@users.noreply.github.com>
2025-05-12 21:52:56 +0900

    [level 1] Title: 최소직사각형, Time: 29.67 ms, Memory: 40.3 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -1,11 +1,20 @@
 function solution(sizes) {
     var answer = 0;
-    let big = [];
-    let small = [];
-    for (let [w,h] of sizes){
-        big.push(Math.max(w,h));
-        small.push(Math.min(w,h)); 
-    }
-    answer = Math.max(...big) * Math.max(...small);
+    // let big = [];
+    // let small = [];
+    // for (let [w,h] of sizes){
+    //     big.push(Math.max(w,h));
+    //     small.push(Math.min(w,h)); 
+    // }
+    // answer = Math.max(...big) * Math.max(...small);
+    const rotated = sizes.map(([w,h])=> w>h ? [w,h] : [h,w]);
+    
+    const widths = rotated.map(([w,h]) => w);
+    const heights = rotated.map(([w,h]) => h);
+    
+    const maxWidth = Math.max(...widths);
+    const maxHeight = Math.max(...heights);
+    answer = maxWidth * maxHeight;
+    
     return answer;
 }
