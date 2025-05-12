fd7a292c9de213a96caa236f2bca6cf2b222cb15
신주현 (Juhyeon Shin) <109642702+Shin363@users.noreply.github.com>
2025-05-13 01:30:07 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -1,11 +1,11 @@
-function isLotto(lotto){
-    if(lotto==6){return 1}
-    else if(lotto==5){return 2}
-    else if(lotto==4){return 3}
-    else if(lotto==3){return 4}
-    else if(lotto==2){return 5}
-    else{return 6}
-}
+// function isLotto(lotto){
+//     if(lotto==6){return 1}
+//     else if(lotto==5){return 2}
+//     else if(lotto==4){return 3}
+//     else if(lotto==3){return 4}
+//     else if(lotto==2){return 5}
+//     else{return 6}
+// }
 function solution(lottos, win_nums) {
     var answer = [0,0];
     lottos.forEach((v)=>{
@@ -17,7 +17,7 @@ function solution(lottos, win_nums) {
             answer[0]++;
         }
     })
-    answer[0]=isLotto(answer[0]);
-    answer[1]=isLotto(answer[1]);
-    return answer;
+    // answer[0]=isLotto(answer[0]);
+    // answer[1]=isLotto(answer[1]);
+    return Array.from(answer,(x)=>x<=1?6:7-x);
 }
