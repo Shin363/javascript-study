ad78d5dd8dd255eda78e773e962296c52bc897f8
신주현 (Juhyeon Shin) <109642702+Shin363@users.noreply.github.com>
2025-05-13 17:07:17 +0900

    [level 1] Title: 음양 더하기, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -1,7 +1,9 @@
 function solution(absolutes, signs) {
     var answer = 0;
-    signs.forEach((v,i)=>{
-        v==true?answer+=absolutes[i]:answer-=absolutes[i];
-    })
-    return answer;
+    // signs.forEach((v,i)=>{
+    //     v==true?answer+=absolutes[i]:answer-=absolutes[i];
+    // })
+    return signs.reduce((acc,cur,i)=> {
+        return acc + (cur?absolutes[i]:-absolutes[i])
+    },0)
 }
