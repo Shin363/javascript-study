2dc0c36454c7b342b7fe620c490ab170e5dfba06
신주현 (Juhyeon Shin) <109642702+Shin363@users.noreply.github.com>
2025-05-15 03:09:19 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -1,14 +1,20 @@
 function solution(n) {
-    var answer = "";
-    let max = 0;
-    while(Math.pow(3,max)<=n){max++;}
-    for(let i=max-1; i>=0; i--){
-        let temp = Math.floor(n/(Math.pow(3,i)));
-        answer += temp.toString();
-        n -= Math.pow(3,i)*temp;
-    }
-    let reverse = [...answer].reverse();
-    return reverse.reduce((acc,cur,i)=>{
-        return acc + Number(cur)*Math.pow(3,max-i-1)
-    },0);
+    // var answer = "";
+    // let max = 0;
+    // while(Math.pow(3,max)<=n){max++;}
+    // for(let i=max-1; i>=0; i--){
+    //     let temp = Math.floor(n/(Math.pow(3,i)));
+    //     answer += temp.toString();
+    //     n -= Math.pow(3,i)*temp;
+    // }
+    // let reverse = [...answer].reverse();
+    // return reverse.reduce((acc,cur,i)=>{
+    //     return acc + Number(cur)*Math.pow(3,max-i-1)
+    // },0);
+    //1. 3진법으로 변환
+    const temp = n.toString(3);
+    //2. 뒤집기
+    const reversed = temp.split("").reverse().join("");
+    //3. 다시 10진수로 변환
+    return parseInt(reversed,3);
 }
