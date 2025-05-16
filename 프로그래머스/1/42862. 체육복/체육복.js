735232bb1dace4e297bb88c32f1113eb90f0faac
신주현 (Juhyeon Shin) <109642702+Shin363@users.noreply.github.com>
2025-05-17 01:36:27 +0900

    [level 1] Title: 체육복, Time: 0.25 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.js
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -1,20 +1,30 @@
 function solution(n, lost, reserve) {
-    var answer = 0;
     const realLost = lost.filter((l)=> !reserve.includes(l));
     const realReserve = reserve.filter((r)=> !lost.includes(r));
+    realReserve.sort((a,b)=>a-b);
+    let lostSet = new Set(realLost);
     
-    realReserve.sort((a, b) => a - b);
-    for(let i=0; i<realReserve.length; i++){
-        if(realLost.length==0) break;
-        else{
-            if(realLost.includes(realReserve[i]-1)||
-                    realLost.includes(realReserve[i]+1)){
-                let left = realLost.indexOf(realReserve[i] - 1);
-                let right = realLost.indexOf(realReserve[i] + 1);
-                let idx2 = left !== -1 ? left : right !== -1 ? right : -1;
-                if (idx2 !== -1) {realLost.splice(idx2, 1);}
-            }
+    for(let r of realReserve){
+        if(lostSet.has(r-1)){
+            lostSet.delete(r-1);
+        }else if(lostSet.has(r+1)){
+            lostSet.delete(r+1);
         }
     }
-    return n-realLost.length;
+    // realReserve.sort((a, b) => a - b);
+    // for(let i=0; i<realReserve.length; i++){
+    //     if(realLost.length==0) break;
+    //     else{
+    //         if(realLost.includes(realReserve[i]-1)||
+    //                 realLost.includes(realReserve[i]+1)){
+    //             let left = realLost.indexOf(realReserve[i] - 1);
+    //             let right = realLost.indexOf(realReserve[i] + 1);
+    //             let idx2 = left !== -1 ? left : right !== -1 ? right : -1;
+    //             if (idx2 !== -1) {realLost.splice(idx2, 1);}
+    //         }
+    //     }
+    // }
+    
+    
+    return n-lostSet.size;
 }
