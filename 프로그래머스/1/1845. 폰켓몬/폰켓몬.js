c8f735ad8ef2c0d0b37cef717db4d689608fe832
신주현 (Juhyeon Shin) <109642702+Shin363@users.noreply.github.com>
2025-05-25 00:33:55 +0900

    [level 1] Title: 폰켓몬, Time: 0.55 ms, Memory: 34.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,13 +1,15 @@
 function solution(nums) {
     var answer = 0;
-    let newMap = new Map();
-    for(let num of nums){
-        if(newMap.has(num)){
-            newMap.set(num,newMap.get(num)+1);
-        }else{
-            newMap.set(num,1);
-        }
-    }
-    answer = newMap.size>=nums.length/2? nums.length/2 : newMap.size;
-    return answer;
+    let newSet = new Set(nums);
+    return Math.min(nums.length/2,newSet.size);
+    // let newMap = new Map();
+    // for(let num of nums){
+    //     if(newMap.has(num)){
+    //         newMap.set(num,newMap.get(num)+1);
+    //     }else{
+    //         newMap.set(num,1);
+    //     }
+    // }
+    // answer = newMap.size>=nums.length/2? nums.length/2 : newMap.size;
+    // return answer;
 }
