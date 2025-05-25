4fd655cc5cb546961bbad0ef00c06e59a9e8b434
신주현 (Juhyeon Shin) <109642702+Shin363@users.noreply.github.com>
2025-05-25 18:44:59 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.60 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -0,0 +1,24 @@
+function solution(board, moves) {
+    var answer = 0;
+    let stacks = Array.from({length:board.length},(v)=>[]) 
+    let tmpStack = [];
+    for(let i=board.length-1; i>=0; i--){
+        for(let j=0; j<board[0].length; j++){
+            if(board[i][j]!==0){
+                stacks[j].push(board[i][j])
+            }
+        }
+    }
+    for(let move of moves){
+        let picked = stacks[move-1].pop();
+        if(picked){
+            if(tmpStack.length>0&&tmpStack[tmpStack.length-1]==picked){
+                tmpStack.pop(); answer+=2;
+            }else{
+                tmpStack.push(picked);
+            }
+        }
+    }
+    
+    return answer;
+}
