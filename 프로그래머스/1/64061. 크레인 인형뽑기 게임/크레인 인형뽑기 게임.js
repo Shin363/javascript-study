85fbc8123fcf64d8761b3e340adae379ddc78ebd
신주현 (Juhyeon Shin) <109642702+Shin363@users.noreply.github.com>
2025-05-25 19:46:36 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 1.27 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -1,24 +1,36 @@
 function solution(board, moves) {
     var answer = 0;
-    let stacks = Array.from({length:board.length},(v)=>[]) 
-    let tmpStack = [];
-    for(let i=board.length-1; i>=0; i--){
-        for(let j=0; j<board[0].length; j++){
-            if(board[i][j]!==0){
-                stacks[j].push(board[i][j])
+    let stacks = [];
+    moves.forEach((move)=>{
+        for(let i=0; i<board.length; i++){
+            if(board[i][move-1]!==0){
+                let tmp = board[i][move-1];
+                board[i][move-1]=0;
+                if(stacks[stacks.length-1]==tmp){
+                    stacks.pop(); answer+=2;
+                }else{stacks.push(tmp)}
+                break;
             }
         }
-    }
-    for(let move of moves){
-        let picked = stacks[move-1].pop();
-        if(picked){
-            if(tmpStack.length>0&&tmpStack[tmpStack.length-1]==picked){
-                tmpStack.pop(); answer+=2;
-            }else{
-                tmpStack.push(picked);
-            }
-        }
-    }
-    
+    })
+    // let stacks = Array.from({length:board.length},(v)=>[]) 
+    // let tmpStack = [];
+    // for(let i=board.length-1; i>=0; i--){
+    //     for(let j=0; j<board[0].length; j++){
+    //         if(board[i][j]!==0){
+    //             stacks[j].push(board[i][j])
+    //         }
+    //     }
+    // }
+    // for(let move of moves){
+    //     let picked = stacks[move-1].pop();
+    //     if(picked){
+    //         if(tmpStack.length>0&&tmpStack[tmpStack.length-1]==picked){
+    //             tmpStack.pop(); answer+=2;
+    //         }else{
+    //             tmpStack.push(picked);
+    //         }
+    //     }
+    // }
     return answer;
 }
