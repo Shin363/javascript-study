e88c106eafc6d291b7d9da1b77a3e6f74cd6d7d5
신주현 (Juhyeon Shin) <109642702+Shin363@users.noreply.github.com>
2025-05-25 13:54:47 +0900

    [level 2] Title: 올바른 괄호, Time: 31.67 ms, Memory: 38 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,10 @@
+function solution(s){
+    var answer = true;
+    let stack = [];
+    for(let x of s){
+        if(x=="(")stack.push(x);
+        else{if(stack.length==0)return false; stack.pop();}
+    }
+    if(stack.length>0)return false;
+    return answer;
+}
