9ede0e14bb73fc8b6dfff582ae290b81ac25317e
신주현 (Juhyeon Shin) <109642702+Shin363@users.noreply.github.com>
2025-05-27 13:50:51 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 36.58 ms, Memory: 83.7 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,9 +1,8 @@
 function solution(arr){
     let answer = [arr[0]];
-    let tmp = arr[0];
     for(let i=1; i<arr.length; i++){
-        if(tmp==arr[i]){continue;}
-        else{answer.push(arr[i]); tmp=arr[i];}
+        if(arr[i-1]!=arr[i]) 
+            answer.push(arr[i]);
     }
     return answer;
 }
