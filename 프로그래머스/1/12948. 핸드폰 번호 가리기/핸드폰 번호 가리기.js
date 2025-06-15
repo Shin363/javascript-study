8674914092f853fb8a184711da6efe3c64abb6b5
신주현 (Juhyeon Shin) <109642702+Shin363@users.noreply.github.com>
2025-06-15 23:01:44 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,8 +1,8 @@
 function solution(phone_number) {
     var answer = '';
-    for(let i=0; i<phone_number.length; i++){
-        answer += i<phone_number.length-4?"*":phone_number[i];
-    }
-    
+    // for(let i=0; i<phone_number.length; i++){
+    //     answer += i<phone_number.length-4?"*":phone_number[i];
+    // }
+    answer = "*".repeat(phone_number.length-4)+phone_number.slice(-4);
     return answer;
 }
