31bf9ab6f102125aae0e10fb5385cbbfdb20f645
Juhyeon Shin <109642702+Shin363@users.noreply.github.com>
2025-10-16 23:09:30 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 2327.73 ms, Memory: 51.8 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -1,3 +1,6 @@
+// 원형 수열을 사용할 땐 배열을 2배로 늘려서 사용하기(인덱스 계산의 편리)
+// 길이가 변하는 모든 부분 수열을 구할 땐 이중 반복문이 효과적
+// 길이가 정해져 있는 부분 수열을 구할 땐 슬라이딩 윈도우 방식이 더 효과적
 function solution(elements) {
     let answer = 0;
     let answerSet = new Set();
@@ -5,6 +8,7 @@ function solution(elements) {
     let sum = 0;
     for(let i=1; i<=elements.length; i++){
         for(let j=0; j<elements.length; j++){
+            //slice와 map을 사용하는 것보다 삼중 반복문이 더 ㄱㅊ을듯
             newElements.slice(j,j+i).map((v,_)=>sum+=v);
             answerSet.add(sum);
             // console.log([...answerSet].sort((a,b)=>a-b));
